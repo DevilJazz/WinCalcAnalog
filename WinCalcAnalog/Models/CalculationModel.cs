0eb03462f2eddac21e47630c01c7119051c662e3
Дмитрий <koval96ua@gmail.com>
2017-10-27 15:44:14 +0300

    FixMess

---
+++ WinCalcAnalog/Models/CalculationModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinCalcAnalog.Models
+{
+    public class CalculationModel
+    {
+        private string result;
+
+        public CalculationModel(string firstOperand, string secondOperand, string operation)
+        {
+            FirstOperand = firstOperand;
+            SecondOperand = secondOperand;
+            Operation = operation;
+            result = string.Empty;
+        }
+
+        public CalculationModel(string firstOperand, string operation)
+        {
+            FirstOperand = firstOperand;
+            SecondOperand = string.Empty;
+            Operation = operation;
+            result = string.Empty;
+        }
+
+        public CalculationModel()
+        {
+            FirstOperand = string.Empty;
+            SecondOperand = string.Empty;
+            Operation = string.Empty;
+            result = string.Empty;
+        }
+
+
+        public string FirstOperand { get; set; }
+        public string SecondOperand { get; set; }
+        public string Operation { get; set; }
+        public string Result { get { return result; } }
+
+        public void CalculateResult()
+        {
+            try
+            {
+                switch (Operation)
+                {
+                    case ("+"):
+                        result = (Convert.ToDouble(FirstOperand) + Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("-"):
+                        result = (Convert.ToDouble(FirstOperand) - Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("*"):
+                        result = (Convert.ToDouble(FirstOperand) * Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case ("/"):
+                        result = (Convert.ToDouble(FirstOperand) / Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+                    case ("1/"):
+                        result = (1 / Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+                    case ("sqr"):
+                        result = Math.Pow(Convert.ToDouble(SecondOperand), 2).ToString();
+                        break;
+                    case ("sqrt"):
+                        result = Math.Sqrt(Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+                    case ("%"):
+                        result = (Convert.ToDouble(FirstOperand) / 100 * Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                result = "Error whilst calculating";
+                throw;
+            }
+        }
+    }
+}
---
+++ WinCalcAnalog/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using WinCalcAnalog.Commands;
+using WinCalcAnalog.Models;
+
+namespace WinCalcAnalog.ViewModels
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private CalculationModel calculation;
+
+        private DelegateCommand<string> digitButtonPressCommand;
+        private DelegateCommand<string> operationButtonPressCommand;
+        
+        private string lastOperation;
+        private bool newDisplayRequired = false;
+        private string fullExpression;
+        private string display;
+
+        public CalculatorViewModel()
+        {
+            calculation = new CalculationModel();
+            display = "0";
+            FirstOperand = string.Empty;
+            SecondOperand = string.Empty;
+            Operation = string.Empty;
+            lastOperation = string.Empty;
+            fullExpression = string.Empty;
+        }
+
+        public string FirstOperand
+        {
+            get { return calculation.FirstOperand; }
+            set { calculation.FirstOperand = value; }
+        }
+
+        public string SecondOperand
+        {
+            get { return calculation.SecondOperand; }
+            set { calculation.SecondOperand = value; }
+        }
+
+        public string Operation
+        {
+            get { return calculation.Operation; }
+            set { calculation.Operation = value; }
+        }
+
+        public string LastOperation
+        {
+            get { return lastOperation; }
+            set { lastOperation = value; }
+        }
+
+        public string Result
+        {
+            get { return calculation.Result; }
+        }
+
+        public string Display
+        {
+            get { return display; }
+            set
+            {
+                display = value;
+                OnPropertyChanged("Display");
+            }
+        }
+
+        public string FullExpression
+        {
+            get { return fullExpression; }
+            set
+            {
+                fullExpression = value;
+                OnPropertyChanged("FullExpression");
+            }
+        }
+
+        public ICommand OperationButtonPressCommand
+        {
+            get
+            {
+                if (operationButtonPressCommand == null)
+                {
+                    operationButtonPressCommand = new DelegateCommand<string>(
+                        OperationButtonPress, (number) => true);
+                }
+                return operationButtonPressCommand;
+            }
+        }
+
+        public ICommand DigitButtonPressCommand
+        {
+            get
+            {
+                if (digitButtonPressCommand == null)
+                {
+                    digitButtonPressCommand = new DelegateCommand<string>(
+                        DigitButtonPress, (number) => true);
+                }
+                return digitButtonPressCommand;
+            }
+        }
+
+        public void DigitButtonPress(string button)
+        {
+            switch (button)
+            {
+                case "CE":
+                    Display = "0";
+                    break;
+                case "C":
+                    Display = "0";
+                    FirstOperand = string.Empty;
+                    SecondOperand = string.Empty;
+                    Operation = string.Empty;
+                    LastOperation = string.Empty;
+                    FullExpression = string.Empty;
+                    break;
+                case "Del":
+                    if (display.Length > 1)
+                        Display = display.Substring(0, display.Length - 1);
+                    else Display = "0";
+                    break;
+                case "+/-":
+                    if (display.Contains("-") || display == "0")
+                    {
+                        Display = display.Remove(display.IndexOf("-"), 1);
+                    }
+                    else Display = "-" + display;
+                    break;
+                case ",":
+                    if (newDisplayRequired)
+                    {
+                        Display = "0,";
+                    }
+                    else
+                    {
+                        if (!display.Contains(","))
+                        {
+                            Display = display + ",";
+                        }
+                    }
+                    break;
+                default:
+                    if (display == "0" || newDisplayRequired)
+                        Display = button;
+                    else
+                        Display = display + button;
+                    break;
+            }
+            newDisplayRequired = false;
+        }
+
+        public void OperationButtonPress(string operation)
+        {
+            try
+            {
+                if (FirstOperand == string.Empty || LastOperation == "=")
+                {
+                    FirstOperand = display;
+                    LastOperation = operation;
+                }
+                else
+                {
+                    SecondOperand = display;
+                    Operation = lastOperation;
+                    calculation.CalculateResult();
+
+
+                    if (Operation == "1/" || Operation == "sqr" || Operation == "sqrt")
+                    {
+                        FullExpression = Operation + "(" +  
+                            Math.Round(Convert.ToDouble(SecondOperand), 10) + ")";
+                    }
+                    else
+                    {
+                        FullExpression = Math.Round(Convert.ToDouble(FirstOperand), 10) + " " + Operation + " "
+                                        + Math.Round(Convert.ToDouble(SecondOperand), 10);
+                    }
+                    LastOperation = operation;
+                    Display = Result;
+                    FirstOperand = display;
+                }
+                newDisplayRequired = true;
+        }
+            catch (Exception)
+            {
+                Display = Result == string.Empty ? "Error" : Result;
+            }
+        }
+    }
+}
